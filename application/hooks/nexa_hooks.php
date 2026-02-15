9cc267ea42e6c0fc04a58e2533fdee570eec0f4d
hasanuzzamanpriyam <h.priyam1999@gmail.com>
2026-02-15 16:27:18 +0600

    space cut

--- application/hooks/nexa_hooks.php
+++ application/hooks/nexa_hooks.php
@@ -2,7 +2,7 @@
 function get_config_items(){
 $CI =& get_instance();
 $session = $CI->load->library('session');
-//$db = $CI->load->library('database');
+// $db = $CI->load->library('database');
 print_r($session); exit;
 $config_items = $db->query("SELECT * FROM tbl_config")->result_array();
 
