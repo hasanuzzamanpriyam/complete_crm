b6ff8534fe756aea215ebf1e823bb0924fa129de
towfiqueemrose <towfiqueemrose@outlook.com>
2026-02-15 11:48:15 +0600

    job posted api intigrated

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,90 +1,175 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 /*
+ 
 | -------------------------------------------------------------------------
+ 
 | URI ROUTING
+ 
 | -------------------------------------------------------------------------
+ 
 | This file lets you re-map URI requests to specific controller functions.
+ 
 |
+ 
 | Typically there is a one-to-one relationship between a URL string
+ 
 | and its corresponding controller class/method. The segments in a
+ 
 | URL normally follow this pattern:
+ 
 |
+ 
 |	example.com/class/method/id/
+ 
 |
+ 
 | In some instances, however, you may want to remap this relationship
+ 
 | so that a different class/function is called than the one
+ 
 | corresponding to the URL.
+ 
 |
+ 
 | Please see the user guide for complete details:
+ 
 |
+ 
 |	http://codeigniter.com/user_guide/general/routing.html
+ 
 |
+ 
 | -------------------------------------------------------------------------
+ 
 | RESERVED ROUTES
+ 
 | -------------------------------------------------------------------------
+ 
 |
+ 
 | There area two reserved routes:
+ 
 |
+ 
 |	$route['default_controller'] = 'welcome';
+ 
 |
+ 
 | This route indicates which controller class should be loaded if the
+ 
 | URI contains no data. In the above example, the "welcome" class
+ 
 | would be loaded.
+ 
 |
+ 
 |	$route['404_override'] = 'errors/page_missing';
+ 
 |
+ 
 | This route will tell the Router what URI segments to use if those provided
+ 
 | in the URL cannot be matched to a valid route.
+ 
 |
+ 
 */
 
 /*
+ 
 | -------------------------------------------------------------------------
+ 
 | URI ROUTING
+ 
 | -------------------------------------------------------------------------
+ 
 | This file lets you re-map URI requests to specific controller functions.
+ 
 |
+ 
 | Typically there is a one-to-one relationship between a URL string
+ 
 | and its corresponding controller class/method. The segments in a
+ 
 | URL normally follow this pattern:
+ 
 |
+ 
 |	example.com/class/method/id/
+ 
 |
+ 
 | In some instances, however, you may want to remap this relationship
+ 
 | so that a different class/function is called than the one
+ 
 | corresponding to the URL.
+ 
 |
+ 
 | Please see the user guide for complete details:
+ 
 |
+ 
 |	https://codeigniter.com/user_guide/general/routing.html
+ 
 |
+ 
 | -------------------------------------------------------------------------
+ 
 | RESERVED ROUTES
+ 
 | -------------------------------------------------------------------------
+ 
 |
+ 
 | There are three reserved routes:
+ 
 |
+ 
 |	$route['default_controller'] = 'welcome';
+ 
 |
+ 
 | This route indicates which controller class should be loaded if the
+ 
 | URI contains no data. In the above example, the "welcome" class
+ 
 | would be loaded.
+ 
 |
+ 
 |	$route['404_override'] = 'errors/page_missing';
+ 
 |
+ 
 | This route will tell the Router which controller/method to use if those
+ 
 | provided in the URL cannot be matched to a valid route.
+ 
 |
+ 
 |	$route['translate_uri_dashes'] = FALSE;
+ 
 |
+ 
 | This is not exactly a route, but allows you to automatically route
+ 
 | controller and method names that contain dashes. '-' isn't a valid
+ 
 | class or method name character, so it requires translation.
+ 
 | When you set this option to TRUE, it will replace ALL dashes in the
+ 
 | controller and method URI segments.
+ 
 |
+ 
 | Examples:	my-controller/index	-> my_controller/index
+ 
 |		my-controller/my-method	-> my_controller/my_method
+ 
 */
 
 $route['default_controller'] = 'login';
@@ -101,12 +186,14 @@ $route['translate_uri_dashes'] = FALSE;
 $modules_path = MODULES_PATH;
 $modules = scandir($modules_path);
 foreach ($modules as $module) {
-    if ($module === '.' || $module === '..' || $module == 'api' || $module == 'frontcms') continue;
+    if ($module === '.' || $module === '..' || $module == 'api' || $module == 'frontcms')
+        continue;
     if (is_dir($modules_path) . '/' . $module) {
         $routes_path = $modules_path . $module . '/config/routes.php';
         if (file_exists($routes_path)) {
             require($routes_path);
-        } else {
+        }
+        else {
             continue;
         }
     }
@@ -116,13 +203,15 @@ foreach ($modules as $module) {
 
 
 /*
+ 
 ------------------------------------------
+ 
 api routes
+ 
 ------------------------------------------
+ 
 */
 
 $route['api/staff-users'] = 'api/staff_users';
 $route['api/clients'] = 'api/clients';
-
-
-
+$route['api/jobs-posted-list'] = 'api/jobs_postedList';
--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -65,4 +65,21 @@ class Api extends CI_Controller
             ->set_status_header(200)
             ->set_output(json_encode(['clients' => $clients]));
     }
+
+    public function jobs_postedList()
+    {
+        $this->db->select('tbl_job_circular.*, tbl_designations.designations');
+        $this->db->from('tbl_job_circular');
+        $this->db->join('tbl_designations', 'tbl_designations.designations_id = tbl_job_circular.designations_id', 'left');
+        $this->db->where('tbl_job_circular.status', 'published');
+        $this->db->order_by('tbl_job_circular.job_circular_id', 'desc');
+
+        $query = $this->db->get();
+        $jobs = $query->result();
+
+        return $this->output
+            ->set_content_type('application/json')
+            ->set_status_header(200)
+            ->set_output(json_encode(['jobs' => $jobs]));
+    }
 }
