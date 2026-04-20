4670a2379a8fe9ecba4ef69b284f48b2c7d754f1
hasanuzzamanpriyam <h.priyam1999@gmail.com>
2026-04-20 10:45:37 +0600

    show times per user

--- application/controllers/admin/Attendance.php
+++ application/controllers/admin/Attendance.php
@@ -1673,7 +1673,15 @@ class Attendance extends Admin_Controller
         $this->db->order_by('max_id', 'DESC');
         $this->db->limit(500);
         
-        $data['all_logs'] = $this->db->get()->result();
+        $all_logs = $this->db->get()->result();
+        
+        foreach ($all_logs as $log) {
+            $log->total_time_seconds = 0;
+            if (!empty($log->clock_in_time) && !empty($log->clock_out_time) && $log->clock_in_time !== $log->clock_out_time) {
+                $log->total_time_seconds = strtotime($log->clock_out_time) - strtotime($log->clock_in_time);
+            }
+        }
+        $data['all_logs'] = $all_logs;
 
         // Get employees for the export dropdown
         $data['all_employee'] = $this->attendance_model->get_all_employee();
@@ -1712,7 +1720,8 @@ class Attendance extends Admin_Controller
             $full_logs[$current_date] = [
                 'log_date' => $current_date,
                 'clock_in_time' => null,
-                'clock_out_time' => null
+                'clock_out_time' => null,
+                'total_time_seconds' => 0
             ];
         }
 
@@ -1732,12 +1741,23 @@ class Attendance extends Admin_Controller
             $this->db->order_by('log_date', 'ASC');
             $logs = $this->db->get('biometric_attendance_logs')->result_array();
 
+            $total_seconds_month = 0;
             foreach ($logs as $log) {
                 if (isset($full_logs[$log['log_date']])) {
                     $full_logs[$log['log_date']]['clock_in_time'] = $log['clock_in_time'];
                     $full_logs[$log['log_date']]['clock_out_time'] = $log['clock_out_time'];
+
+                    $total_time_seconds = 0;
+                    if (!empty($log['clock_in_time']) && !empty($log['clock_out_time']) && $log['clock_in_time'] !== $log['clock_out_time']) {
+                        $total_time_seconds = strtotime($log['clock_out_time']) - strtotime($log['clock_in_time']);
+                    }
+                    $full_logs[$log['log_date']]['total_time_seconds'] = $total_time_seconds;
+                    $total_seconds_month += $total_time_seconds;
                 }
             }
+            $data['total_seconds_month'] = $total_seconds_month;
+        } else {
+            $data['total_seconds_month'] = 0;
         }
 
         $logs_array = array_values($full_logs);
@@ -1799,7 +1819,8 @@ class Attendance extends Admin_Controller
             $full_logs[$current_date] = [
                 'log_date' => $current_date,
                 'clock_in_time' => null,
-                'clock_out_time' => null
+                'clock_out_time' => null,
+                'total_time_seconds' => 0
             ];
         }
 
@@ -1823,10 +1844,18 @@ class Attendance extends Admin_Controller
         $this->db->order_by('log_date', 'ASC');
         $logs = $this->db->get('biometric_attendance_logs')->result_array();
 
+        $total_seconds_month = 0;
         foreach ($logs as $log) {
             if (isset($full_logs[$log['log_date']])) {
                 $full_logs[$log['log_date']]['clock_in_time'] = $log['clock_in_time'];
                 $full_logs[$log['log_date']]['clock_out_time'] = $log['clock_out_time'];
+
+                $total_time_seconds = 0;
+                if (!empty($log['clock_in_time']) && !empty($log['clock_out_time']) && $log['clock_in_time'] !== $log['clock_out_time']) {
+                    $total_time_seconds = strtotime($log['clock_out_time']) - strtotime($log['clock_in_time']);
+                }
+                $full_logs[$log['log_date']]['total_time_seconds'] = $total_time_seconds;
+                $total_seconds_month += $total_time_seconds;
             }
         }
         $logs = array_values($full_logs);
@@ -1849,16 +1878,24 @@ class Attendance extends Admin_Controller
         fputcsv($file, array("")); // Empty line separator
         
         // Write Data Table Header
-        $header = array("Date", "Clock In Time", "Clock Out Time", "Status");
+        $header = array("Date", "Clock In Time", "Clock Out Time", "Total Time", "Status");
         fputcsv($file, $header);
         
         if (!empty($logs)) {
             foreach ($logs as $log) {
+                $time_str = '-';
+                if ($log['total_time_seconds'] > 0) {
+                    $hours = floor($log['total_time_seconds'] / 3600);
+                    $minutes = floor(($log['total_time_seconds'] % 3600) / 60);
+                    $time_str = $hours . "h " . $minutes . "m";
+                }
+
                 if(empty($log['clock_in_time'])) {
                     fputcsv($file, array(
                         date('d M, Y', strtotime($log['log_date'])),
                         '-',
                         '-',
+                        $time_str,
                         'Absent / No Log'
                     ));
                 } else {
@@ -1866,10 +1903,17 @@ class Attendance extends Admin_Controller
                         date('d M, Y', strtotime($log['log_date'])),
                         date('h:i:s A', strtotime($log['clock_in_time'])),
                         $log['clock_in_time'] === $log['clock_out_time'] ? '-' : date('h:i:s A', strtotime($log['clock_out_time'])),
+                        $time_str,
                         'Present'
                     ));
                 }
             }
+
+            // Write Monthly Total Row
+            $m_hours = floor($total_seconds_month / 3600);
+            $m_minutes = floor(($total_seconds_month % 3600) / 60);
+            fputcsv($file, array(""));
+            fputcsv($file, array("TOTAL WORKED TIME FOR MONTH:", "", "", $m_hours . "h " . $m_minutes . "m", ""));
         }
         
         fclose($file);
--- application/views/admin/attendance/view_monthly_raw_logs.php
+++ application/views/admin/attendance/view_monthly_raw_logs.php
@@ -55,10 +55,11 @@
                     <table class="table table-striped table-bordered" id="monthly_log_table" width="100%">
                         <thead>
                             <tr>
-                                <th style="width: 20%;">Date</th>
-                                <th style="width: 30%;">Clock In Time</th>
-                                <th style="width: 30%;">Clock Out Time</th>
-                                <th style="width: 20%;">Status</th>
+                                <th style="width: 15%;">Date</th>
+                                <th style="width: 25%;">Clock In Time</th>
+                                <th style="width: 25%;">Clock Out Time</th>
+                                <th style="width: 20%;">Total Time</th>
+                                <th style="width: 15%;">Status</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -66,6 +67,7 @@
                             <tr class="<?= !empty($log['clock_in_time']) ? 'success' : '' ?>">
                                 <td><strong><?= date('d M, Y', strtotime($log['log_date'])) ?></strong></td>
                                 <?php if(empty($log['clock_in_time'])): ?>
+                                    <td class="text-center">-</td>
                                     <td class="text-center">-</td>
                                     <td class="text-center">-</td>
                                     <td><span class="label label-danger">Absent / No Log</span></td>
@@ -78,10 +80,36 @@
                                             <span class="text-warning"><?= date('h:i:s A', strtotime($log['clock_out_time'])) ?></span>
                                         <?php endif; ?>
                                     </td>
+                                    <td>
+                                        <?php 
+                                            if ($log['total_time_seconds'] > 0) {
+                                                $hours = floor($log['total_time_seconds'] / 3600);
+                                                $minutes = floor(($log['total_time_seconds'] % 3600) / 60);
+                                                echo "<span class='text-info'><strong>{$hours}h {$minutes}m</strong></span>";
+                                            } else {
+                                                echo "<span class='text-muted'>-</span>";
+                                            }
+                                        ?>
+                                    </td>
                                     <td><span class="label label-success">Present</span></td>
                                 <?php endif; ?>
                             </tr>
-                            <?php endforeach; else: ?>
+                            <?php endforeach; ?>
+                            <!-- Total Row -->
+                            <tr class="info" style="font-size: 16px;">
+                                <td colspan="3" class="text-right"><strong>MONTHLY TOTAL WORKED TIME:</strong></td>
+                                <td>
+                                    <strong>
+                                        <?php 
+                                            $m_hours = floor($total_seconds_month / 3600);
+                                            $m_minutes = floor(($total_seconds_month % 3600) / 60);
+                                            echo "{$m_hours}h {$m_minutes}m";
+                                        ?>
+                                    </strong>
+                                </td>
+                                <td></td>
+                            </tr>
+                            <?php else: ?>
                             <tr>
                                 <td colspan="4" class="text-center">No attendance logs found for this month.</td>
                             </tr>
