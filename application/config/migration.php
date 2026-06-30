57e4948ed7bb2ebd06270d800dde6dd27919f11c
hasanuzzamanpriyam <h.priyam1999@gmail.com>
2026-06-30 12:18:33 +0600

    fix migration gap: rename 999-1004 to 633-638, set version to 638

--- application/config/migration.php
+++ application/config/migration.php
@@ -67,7 +67,7 @@ $config['migration_auto_latest'] = TRUE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 1004;
+$config['migration_version'] = 638;
 /*
 |--------------------------------------------------------------------------
 | Migrations Path
